e7a32ed8d1e74c3265ce886ead346dd23d892ed4
Viet Vo <viet.allen.qvo@gmail.com>
2020-02-11 00:08:50 -0800

    unknown changes 2/11

---
+++ client/src/pages/MainPage/MainPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+export default function MainPage() {
+  return (
+    <div>
+      <Grid container direction="column">
+        <Grid item>1</Grid>
+        <Grid item>2</Grid>
+        <Grid item>3</Grid>
+      </Grid>
+    </div>
+  );
+}
